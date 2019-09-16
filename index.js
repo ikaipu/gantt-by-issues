319b71303d03e5fd04cf609a6bbf580405c5b7de
ikaipu <kabikabitznz@gmail.com>
2019-09-16 19:29:25 +0800

    support for es6

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-require('dotenv').config()
-const Octokit = require('@octokit/rest');
+import dotenv from 'dotenv';
+import Octokit from '@octokit/rest';
+
+dotenv.config()
 const octokit = new Octokit({auth: process.env.TOKEN});
+
 
 // Compare: https://developer.github.com/v3/repos/#list-organization-repositories
 octokit.issues.listForRepo({
   owner: 'papuaaaaaaa',
   repo: 'alc-tablet-typeA'
-}).then(({ issues }) => {
+}).then(( {data} ) => {
   // handle data
-  console.log(issues);
+  console.log(data);
+}).catch(e => {
+  console.error(e);
 });
